aff097a877030b74eb690ebdcd97da2f6c7db141
mmmforfire <monica.mackenzie@fdacs.gov>
2024-06-11 16:50:28 -0400

    hope this works!

---
+++ script.js
@@ -0,0 +1,12 @@
+window.addEventListener("load", function() {
+  const form = document.getElementById('digital-log');
+  form.addEventListener("submit", function(e) {
+    e.preventDefault();
+    const data = new FormData(form);
+    const action = e.target.action;
+    fetch(action, {
+      method: 'POST',
+      body: data
+    })
+  });
+});
